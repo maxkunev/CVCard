779600acb0ba8a3f48c566f9247040117211b77f
maxkuniev <yoggi2004@gmail.com>
2026-06-08 23:57:19 +0300

    hotfix

--- main/views.py
+++ main/views.py
@@ -7,44 +7,18 @@ from django.contrib import messages
 # Create your views here.
 
 def index(request):
-    cvprofile = cache.get("cvprofile")
-    if not cvprofile:
-        cvprofile = CVProfile.objects.first()
-        cache.set("cvprofile", cvprofile, 60 * 60 * 24)
-
-    cvskills = cache.get("cvskills")
-    if not cvskills:
-        cvskills = list(CVSkill.objects.all())
-        cache.set("cvskills", cvskills, 60 * 60 * 24)
-
-    cvexperiences = cache.get("cvexperiences")
-    if not cvexperiences:
-        cvexperiences = list(CVExperience.objects.all())
-        cache.set("cvexperiences", cvexperiences, 60 * 60 * 24)
-
-    cvpetprojects = cache.get("cvpetprojects")
-    if not cvpetprojects:
-        cvpetprojects = list(CVPetProject.objects.all())
-        cache.set("cvpetprojects", cvpetprojects, 60 * 60 * 24)
-
-    cveducations = cache.get("cveducations")
-    if not cveducations:
-        cveducations = list(CVEducation.objects.all())
-        cache.set("cveducations", cveducations, 60 * 60 * 24)
-
-    cvcourses = cache.get("cvcourses")
-    if not cvcourses:
-        cvcourses = list(CVCourses.objects.all())
-        cache.set("cvcourses", cvcourses, 60 * 60 * 24)
+    context = cache.get("cv_full_context")
     
-    context = {
-        "cvprofile":cvprofile,
-        "cvskills":cvskills,
-        "cvexperiences":cvexperiences,
-        "cvpetprojects":cvpetprojects,
-        "cveducations":cveducations,
-        "cvcourses":cvcourses
-    }
+    if not context:
+        context = {
+            "cvprofile": CVProfile.objects.first(),
+            "cvskills": list(CVSkill.objects.all()),
+            "cvexperiences": list(CVExperience.objects.all()),
+            "cvpetprojects": list(CVPetProject.objects.all()),
+            "cveducations": list(CVEducation.objects.all()),
+            "cvcourses": list(CVCourses.objects.all()),
+        }
+        cache.set("cv_full_context", context, 60 * 60 * 24)
     
     return render(request, "main/index.html", context=context)
 
